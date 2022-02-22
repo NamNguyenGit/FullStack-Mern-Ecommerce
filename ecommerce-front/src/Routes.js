201ed5fecedb4a69134e72f9bd8de0b0c05784e7
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-22 17:14:25 +0700

    router

--- ecommerce-front/src/Routes.js
+++ ecommerce-front/src/Routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignIn from "./user/Signin";
 import SignUp from "./user/Signup";
+import Home from "./cores/Home";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/" exact component={Home} />
         <Route path="/signIn" exact component={SignIn} />
         <Route path="/signUp" exact component={SignUp} />
       </Switch>
