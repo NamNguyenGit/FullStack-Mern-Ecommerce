18a77504870cc01f1ec18368ae742e1a576ae492
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-24 09:48:22 +0700

    Search form

--- ecommerce-front/src/cores/Search.js
+++ ecommerce-front/src/cores/Search.js
@@ -1,39 +1,78 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getCategories } from "./apiCore";
 
 const Search = () => {
-    const [data, setData] = useState({
-        categories: [],
-        category: '',
-        search:'',
-        results: [],
-        searched: false
-    })
+  const [data, setData] = useState({
+    categories: [],
+    category: "",
+    search: "",
+    results: [],
+    searched: false,
+  });
 
-    const loadCategories = () => {
-        getCategories().then(data => {
-            if(data.error) {
-                console.log(data.error)
-            }
-            else {
-                setData({...data, categories: data})
-            }
-        })
-    }
+  const loadCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setData({ ...data, categories: data });
+      }
+    });
+  };
 
-    const {categories,category,search,results,searched} = data;
+  const { categories, category, search, results, searched } = data;
 
-    useEffect(() => {
-        loadCategories()
-    }, [])
+  useEffect(() => {
+    loadCategories();
+  }, []);
 
+  const searchSubmit = () => {};
+
+  const handleChange = () => {};
+
+  const searchForm = () => {
     return (
-        <>
-        <div>
-            <h2>Search bar {JSON.stringify(categories)}</h2>
-        </div>
-        </>
-      );
-}
- 
+      <>
+        <form onSubmit={searchSubmit}>
+          <span className="input-group-text">
+            <div className="input-group input-group-lg">
+              <div className="input-group-prepend">
+                <select
+                  className="btn mr-2"
+                  onChange={handleChange("category")}
+                >
+                  <option value="All"> Pick Category</option>
+                  {categories.map((c, i) => (
+                    <option key={i} value={c.id}>
+                      {c.name}{" "}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <input
+                type="search"
+                className="form-control"
+                onChange={handleChange("search")}
+                placeholder="Search by name"
+              />
+            </div>
+            <div className="btn input-group-append" style={{ border: "none" }}>
+              <button className="input-group-text">Search</button>
+            </div>
+          </span>
+        </form>
+        ;
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div>
+        <div className="container mb-3">{searchForm()}</div>
+      </div>
+    </>
+  );
+};
+
 export default Search;
