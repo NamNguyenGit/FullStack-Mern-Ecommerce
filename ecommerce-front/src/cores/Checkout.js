6656b6c8cb80c07399dceaac1f4e440d7491640a
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-24 18:13:44 +0700

    Braintree setup frontend

--- ecommerce-front/src/cores/Checkout.js
+++ ecommerce-front/src/cores/Checkout.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { isAuthenticated } from "../auth";
+import { getBraintreeClientToken } from "./apiCore";
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = ({ products }) => {
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {},
+    address: "",
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then((data) => {
+      if (data.error) {
+        setData({ ...data, error: data.error });
+      } else {
+        setData({ ...data, clientToken: data.clientToken });
+      }
+    });
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
@@ -9,18 +37,34 @@ const Checkout = ({ products }) => {
   };
 
   const showCheckout = () => {
-    return (
-      <>
-        {isAuthenticated() ? (
-          <button className="btn btn-success"> Checkout</button>
-        ) : (
-          <Link to="/signIn">
-            <button className="btn btn-primary"> Sign in to checkout</button>
-          </Link>
-        )}
-      </>
+    return isAuthenticated() ? (
+      <div>{showDropIn()}</div>
+    ) : (
+      <Link to="/signIn">
+        <button className="btn btn-warning">Sign In to checkout</button>
+      </Link>
     );
   };
+
+  const showDropIn = () => (
+    
+        <div>
+          {data.clientToken !== null && products.length > 0 ? (
+            <div>
+              <DropIn
+                options={{
+                  authorization: data.clientToken,
+                }}
+                onInstance={(instance) => (data.instance = instance)}
+              />
+              <button className="btn btn-success">Checkout</button>
+            </div>
+          ) : null}
+        </div>
+      
+   
+  );
+
   return (
     <>
       <h2>
