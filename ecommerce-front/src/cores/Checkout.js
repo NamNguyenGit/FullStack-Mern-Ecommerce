27e27aa7e206f1c418aadacd92cb2208f228ffee
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-25 12:37:59 +0700

    Save delivery address of orders

--- ecommerce-front/src/cores/Checkout.js
+++ ecommerce-front/src/cores/Checkout.js
@@ -49,6 +49,8 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
     );
   };
 
+  let deliveryAddress = data.address
+
   const buy = () => {
     setData({ loading: true });
     // send the nonce to your server
@@ -73,7 +75,7 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
               products: products,
               transaction_id: response.transaction.id,
               amount: response.transaction.amount,
-              address: data.address,
+              address: deliveryAddress,
             };
 
 
